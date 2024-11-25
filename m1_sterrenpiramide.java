dd192fc4910acbcd215e44b24b8732e1d92cc203
Hublack <71221641+HublackBE@users.noreply.github.com>
2024-11-25 11:10:09 +0100

    feat(sterrenpiramide()): add dynamic height based on input from user

--- m1_sterrenpiramide.java
+++ m1_sterrenpiramide.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class m1_sterrenpiramide {
     public static void sterrenpiramide() {
-        short hoogte = 5;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Geef een hoogte in: ");
+        short hoogte = reader.nextShort();
         for (int i = 0; i < hoogte; i++) {
             System.out.println("*".repeat(i+1));
         }
